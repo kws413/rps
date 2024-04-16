75c2b00063e0bc542584ac49bf24b3d12b676216
kws413 <ks600146@wne.edu>
2024-04-16 11:21:19 -0400

    play button inserted

--- script.js
+++ script.js
@@ -18,15 +18,27 @@ function main() {
         }
         return 'You lose!';
     }
+
+    let playerChoice = "";
     
     document.getElementById('choices').addEventListener('click', function(event) {
-        const playerChoice = event.target.id;
+        playerChoice = event.target.id;
+        document.getElementById('playerChoice').textContent = playerChoice;
+    });
+
+    document.getElementById('play').addEventListener('click', function(event) {
         const computerChoice = getComputerChoice();
         const result = determineWinner(playerChoice, computerChoice);
     
-        document.getElementById('playerChoice').textContent = playerChoice;
         document.getElementById('computerChoice').textContent = computerChoice;
         document.getElementById('result').textContent = result;
+        if (result === 'You win!'){
+            document.getElementById('result').style.backgroundColor = 'green';
+        } else if (result === 'You lose!') {
+            document.getElementById('result').style.backgroundColor = 'red';
+        } else {
+            document.getElementById('result').style.backgroundColor = 'white';
+        }
     });
 }
 
